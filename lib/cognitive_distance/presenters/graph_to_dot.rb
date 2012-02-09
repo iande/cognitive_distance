cc281b9670c39fde0de02bfd713ed55cd265e223
Ian D. Eccles <ian.eccles@gmail.com>
2012-02-09 08:54:03 -0500

    templatize digraph output
    * add `resources` directory to hold our templates, assets, etc
    * update GraphToDot presenter to use digraph template and ERB

--- lib/cognitive_distance/presenters/graph_to_dot.rb
+++ lib/cognitive_distance/presenters/graph_to_dot.rb
@@ -1,10 +1,15 @@
+require 'erb'
+
 module CognitiveDistance::Presenters
   class GraphToDot
-    def present graph, name='graphname', &label
-      label ||= lambda { |v| v.to_s }
-      "digraph #{name.to_s.inspect} {\n" + graph.inject("") { |str, (v1,v2)|
-        str << "#{label[v1].inspect} -> #{label[v2].inspect};\n"
-      } + "}"
+    def template_file
+      File.expand_path('../../../../resources/templates/digraph_dot.erb', __FILE__)
+    end
+
+    def present graph, name='graphname', &labeler
+      labeler ||= lambda { |v| v.to_s }
+      template = ERB.new(File.read(template_file))
+      template.result binding
     end
   end
 end
--- spec/cognitive_distance/presenters/graph_to_dot_spec.rb
+++ spec/cognitive_distance/presenters/graph_to_dot_spec.rb
@@ -10,17 +10,26 @@ describe CognitiveDistance::Presenters::GraphToDot do
 
   it "converts a graph to dot format" do
     dotted = @presenter.present @graph
-    dotted.must_equal "digraph \"graphname\" {\n\"a\" -> \"b\";\n\"a\" -> \"c\";\n\"b\" -> \"c\";\n}"
+    dotted.must_match /^digraph "graphname"/
+    dotted.must_match /"a" -> "b";/
+    dotted.must_match /"a" -> "c";/
+    dotted.must_match /"b" -> "c";/
   end
 
   it "names the graph" do
-    dotted = @presenter.present @graph, "MyGraph"
-    dotted.must_equal "digraph \"MyGraph\" {\n\"a\" -> \"b\";\n\"a\" -> \"c\";\n\"b\" -> \"c\";\n}"
+    dotted = @presenter.present @graph, "My Graph"
+    dotted.must_match /^digraph "My Graph"/
+    dotted.must_match /"a" -> "b";/
+    dotted.must_match /"a" -> "c";/
+    dotted.must_match /"b" -> "c";/
   end
 
   it "uses a block to map nodes to names" do
     dotted = @presenter.present(@graph, "Blocked!") { |v| "Node #{v}" }
-    dotted.must_equal "digraph \"Blocked!\" {\n\"Node a\" -> \"Node b\";\n\"Node a\" -> \"Node c\";\n\"Node b\" -> \"Node c\";\n}"
+    dotted.must_match /^digraph "Blocked!"/
+    dotted.must_match /"Node a" -> "Node b";/
+    dotted.must_match /"Node a" -> "Node c";/
+    dotted.must_match /"Node b" -> "Node c";/
   end
 end
 
