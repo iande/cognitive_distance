2a5e2b33f429143b5c6d95d27b42b34742c77e13
Ian D. Eccles <ian.eccles@gmail.com>
2012-02-27 13:48:05 -0500

    Clean up a few more specs

--- spec/cognitive_distance/structures/call_tree_spec.rb
+++ spec/cognitive_distance/structures/call_tree_spec.rb
@@ -1,55 +1,56 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe CognitiveDistance::Structures::CallTree do
-  before do
-    @call_tree = CognitiveDistance::Structures::CallTree.new
-  end
+module CognitiveDistance
+  describe Structures::CallTree do
+    before do
+      @call_tree = CognitiveDistance::Structures::CallTree.new
+    end
 
-  def populate_tree
-    @call_tree.called 1, nil, nil, nil, nil
-    @call_tree.called 11, nil, nil, nil, nil
-    @call_tree.returned 11, nil, nil, nil, nil
-    @call_tree.called 12, nil, nil, nil, nil
-    @call_tree.returned 12, nil, nil, nil, nil
-    @call_tree.returned 1, nil, nil, nil, nil
-    @call_tree.called 2, nil, nil, nil, nil
-    @call_tree.called 21, nil, nil, nil, nil
-    @call_tree.returned 21, nil, nil, nil, nil
-    @call_tree.returned 2, nil, nil, nil, nil
-    @call_tree.called 3, nil, nil, nil, nil
-    @call_tree.returned 3, nil, nil, nil, nil
-  end
+    it "returns the root node" do
+      @call_tree.root.
+        is_a?(CognitiveDistance::Structures::CallNodeRoot).must_equal true
+    end
 
-  it "returns the root node" do
-    @call_tree.root.
-      is_a?(CognitiveDistance::Structures::CallNodeRoot).must_equal true
-  end
+    it "is empty when there's nothing in the tree" do
+      @call_tree.empty?.must_equal true
+      @call_tree.called nil, nil, nil, nil, nil
+      @call_tree.empty?.must_equal false
+    end
 
-  it "is empty when there's nothing in the tree" do
-    @call_tree.empty?.must_equal true
-    @call_tree.called nil, nil, nil, nil, nil
-    @call_tree.empty?.must_equal false
-  end
+    it "converts to an array by converting the root node" do
+      populate_tree
+      @call_tree.to_a.must_equal @call_tree.root.to_a
+    end
 
-  it "converts to an array by converting the root node" do
-    populate_tree
-    @call_tree.to_a.must_equal @call_tree.root.to_a
-  end
+    it "contains all nodes when enumerated" do
+      populate_tree
+      @call_tree.map(&:trace_class).must_equal [1, 11, 12, 2, 21, 3]
+    end
 
-  it "contains all nodes when enumerated" do
-    populate_tree
-    @call_tree.map(&:trace_class).must_equal [1, 11, 12, 2, 21, 3]
-  end
+    it "calculates size from the children of the root" do
+      populate_tree
+      @call_tree.size.must_equal 6
+    end
 
-  it "calculates size from the children of the root" do
-    populate_tree
-    @call_tree.size.must_equal 6
-  end
+    it "freezes all nodes when freezing the tree" do
+      populate_tree
+      @call_tree.freeze
+      @call_tree.all?(&:frozen?).must_equal true
+    end
 
-  it "freezes all nodes when freezing the tree" do
-    populate_tree
-    @call_tree.freeze
-    @call_tree.all?(&:frozen?).must_equal true
+    def populate_tree
+      @call_tree.called 1, nil, nil, nil, nil
+      @call_tree.called 11, nil, nil, nil, nil
+      @call_tree.returned 11, nil, nil, nil, nil
+      @call_tree.called 12, nil, nil, nil, nil
+      @call_tree.returned 12, nil, nil, nil, nil
+      @call_tree.returned 1, nil, nil, nil, nil
+      @call_tree.called 2, nil, nil, nil, nil
+      @call_tree.called 21, nil, nil, nil, nil
+      @call_tree.returned 21, nil, nil, nil, nil
+      @call_tree.returned 2, nil, nil, nil, nil
+      @call_tree.called 3, nil, nil, nil, nil
+      @call_tree.returned 3, nil, nil, nil, nil
+    end
   end
 end
-
--- spec/cognitive_distance/transforms/call_tree_to_module_boundary_graph_spec.rb
+++ spec/cognitive_distance/transforms/call_tree_to_module_boundary_graph_spec.rb
@@ -1,57 +1,58 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe CognitiveDistance::Transforms::CallTreeToModuleBoundaryGraph do
-  def make_binding
-    [].method(:length).to_proc.binding
-  end
+module CognitiveDistance
+  describe Transforms::CallTreeToModuleBoundaryGraph do
+    before do
+      @transformer = Transforms::CallTreeToModuleBoundaryGraph.new
+      # We're specifically transforming a call tree, so let's use a real one
+      @tree = make_tree
+    end
 
-  def make_tree
-    b1, b2, b3, b4 = 4.times.map { make_binding }
-    nils = [nil, nil, nil]
-    CognitiveDistance::Structures::CallTree.new.tap do |tree|
-      tree.called 1, *nils, b1
-      tree.called 2, *nils, b3
-      tree.returned 2, *nils, b3 # => 1
-      tree.called 3, *nils, b1
-      tree.called 4, *nils, b1
-      tree.called 5, *nils, b4
-      tree.returned 5, *nils, b4 # => 4
-      tree.returned 4, *nils, b1 # => 3
-      tree.returned 3, *nils, b1 # => 1
-      tree.returned 1, *nils, b1 # => ROOT
+    it "transforms a CallTree into a graph of boundary crossings" do
+      graph = @transformer.transform(@tree)
+      graph.vertices.size.must_equal 8
+      graph.edges.size.must_equal 4
+      mapped = graph.map { |(v1,v2)| [v1.trace_class, v2.trace_class] }
+      mapped.must_include [1, 2]
+      mapped.must_include [4, 5]
+      mapped.must_include [20, 23]
+      mapped.must_include [21, 22]
+    end
 
-      tree.called 20, *nils, b2
-      tree.called 21, *nils, b2
-      tree.called 22, *nils, b1
-      tree.returned 22, *nils, b1 # => 21
-      tree.returned 21, *nils, b2 # => 20
-      tree.called 23, *nils, b1
-      tree.returned 23, *nils, b1 # => 20
-      tree.returned 20, *nils, b2 # => ROOT
+    it "provides a singleton convenience method" do
+      graph1 = @transformer.transform(@tree)
+      graph2 = Transforms::CallTreeToModuleBoundaryGraph.transform(@tree)
+      graph1.must_equal graph2
+    end
+    
+    def make_binding
+      [].method(:length).to_proc.binding
+    end
+
+    def make_tree
+      b1, b2, b3, b4 = 4.times.map { make_binding }
+      nils = [nil, nil, nil]
+      Structures::CallTree.new.tap do |tree|
+        tree.called 1, *nils, b1
+        tree.called 2, *nils, b3
+        tree.returned 2, *nils, b3 # => 1
+        tree.called 3, *nils, b1
+        tree.called 4, *nils, b1
+        tree.called 5, *nils, b4
+        tree.returned 5, *nils, b4 # => 4
+        tree.returned 4, *nils, b1 # => 3
+        tree.returned 3, *nils, b1 # => 1
+        tree.returned 1, *nils, b1 # => ROOT
+
+        tree.called 20, *nils, b2
+        tree.called 21, *nils, b2
+        tree.called 22, *nils, b1
+        tree.returned 22, *nils, b1 # => 21
+        tree.returned 21, *nils, b2 # => 20
+        tree.called 23, *nils, b1
+        tree.returned 23, *nils, b1 # => 20
+        tree.returned 20, *nils, b2 # => ROOT
+      end
     end
   end
-
-  before do
-    @transformer = CognitiveDistance::Transforms::CallTreeToModuleBoundaryGraph.new
-    # We're specifically transforming a call tree, so let's use a real one
-    @tree = make_tree
-  end
-
-  it "transforms a CallTree into a graph of boundary crossings" do
-    graph = @transformer.transform(@tree)
-    graph.vertices.size.must_equal 8
-    graph.edges.size.must_equal 4
-    mapped = graph.map { |(v1,v2)| [v1.trace_class, v2.trace_class] }
-    mapped.must_include [1, 2]
-    mapped.must_include [4, 5]
-    mapped.must_include [20, 23]
-    mapped.must_include [21, 22]
-  end
-
-  it "provides a singleton convenience method" do
-    graph1 = @transformer.transform(@tree)
-    graph2 = CognitiveDistance::Transforms::CallTreeToModuleBoundaryGraph.transform(@tree)
-    graph1.must_equal graph2
-  end
 end
-
